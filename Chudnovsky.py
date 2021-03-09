6c302c682049606128c6370bdbaadb176022bfc5
joaojcorreia <joaojcorreia@gmail.com>
2021-03-09 23:27:13 +0000

    Several changes - logs data in to files, requests the number of decimal spaces - only on the chudnvosky algorithm

--- Chudnovsky.py
+++ Chudnovsky.py
@@ -1,8 +1,11 @@
 import decimal
 import math
+import time
 
+p2rint ('Please input the numember of decimal spaces')
+dec = int(input())
 
-decimal.getcontext().prec = 100000
+decimal.getcontext().prec = dec
 C = decimal.Decimal(426880*math.sqrt(10005))
 
 u=decimal.Decimal(0)
@@ -12,18 +15,22 @@ pi = decimal.Decimal(3)
 
 q = int(0)
 
+with open ('PI_logs/' + time.strftime("%Y%m%d-%H%M") + '.txt','w+') as text_file:
+    print('start time: ', time.strftime("%Y%m%d-%H%M%S" ), file=text_file)
+    while u != oldu:
+        
+        oldu = u
 
-while u != oldu:
-    
-    oldu = u
+        u += decimal.Decimal(((decimal.Decimal(math.factorial(6*q))/(decimal.Decimal(math.factorial(3*q))*decimal.Decimal((math.factorial(q))**3)))*(545140134*q+13591409))/(decimal.Decimal((-262537412640768000)**q)))
 
-    u += decimal.Decimal(((decimal.Decimal(math.factorial(6*q))/(decimal.Decimal(math.factorial(3*q))*decimal.Decimal((math.factorial(q))**3)))*(545140134*q+13591409))/(decimal.Decimal((-262537412640768000)**q)))
-
-    q += 1
-
-    print (q)
+        q += 1
 
 
-pi = C/u
 
-print(pi)
+    pi = C/u
+
+    print('end time: ', time.strftime("%Y%m%d-%H%M%S"), file=text_file)
+    print('interactions: ', (q-1), file=text_file)
+    print('pi: ', (pi), file=text_file)
+
+    print(pi)
